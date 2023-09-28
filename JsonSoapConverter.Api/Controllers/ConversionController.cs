acd36b7ad1a5a907ffea7623ec2001b018bc072c
pjjdebruijn <66751819+pjjdebruijn@users.noreply.github.com>
2023-09-29 01:39:10 +0200

    added response schemas, and some attributes on parameters, we will need xsds not xml's to get all the attributes...

--- JsonSoapConverter.Api/Controllers/ConversionController.cs
+++ JsonSoapConverter.Api/Controllers/ConversionController.cs
@@ -1,11 +1,14 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+using System.Xml;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JsonSoapConverter.Api
 {
     [ApiController]
     [Route("[controller]")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(NumberToWordsResponse))]
     public class NumberConversionController : ControllerBase
     {
         /// <summary>
@@ -17,7 +20,8 @@ namespace JsonSoapConverter.Api
         [Route("NumberToWords")]
         public async Task<IActionResult> NumberToWords([FromBody] NumberToWordsRequest request)
         {
-            var soapRequestBody = 
+            // to get this result we need a function that takes an example soap request and parameterizes the variables (and set the default vale to the original value in the soap example request)
+            string soapRequestBody = 
 $@"<?xml version=""1.0"" encoding=""utf-8""?>
 <soap:Envelope xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"">
     <soap:Body>
@@ -28,19 +32,19 @@ $@"<?xml version=""1.0"" encoding=""utf-8""?>
 </soap:Envelope>";
 
             using HttpClient httpClient = new HttpClient();
-            using var content = new StringContent(soapRequestBody, Encoding.UTF8, "text/xml");
-            using var response = await httpClient.PostAsync("https://www.dataaccess.com/webservicesserver/NumberConversion.wso", content);
-            var soapResponse = await response.Content.ReadAsStringAsync();
+            using StringContent content = new StringContent(soapRequestBody, Encoding.UTF8, "text/xml");
+            using HttpResponseMessage response = await httpClient.PostAsync("https://www.dataaccess.com/webservicesserver/NumberConversion.wso", content);
+            string soapResponse = await response.Content.ReadAsStringAsync();
 
-            var startTag = "<m:NumberToWordsResult>";
-            var endTag = "</m:NumberToWordsResult>";
-            var start = soapResponse.IndexOf(startTag);
-            var end = soapResponse.IndexOf(endTag);
+            string startTag = "<m:NumberToWordsResult>";
+            string endTag = "</m:NumberToWordsResult>";
+            int start = soapResponse.IndexOf(startTag);
+            int end = soapResponse.IndexOf(endTag);
 
             if (start != -1 && end != -1)
             {
                 start += startTag.Length;
-                var result = soapResponse[start..end].Trim();
+                string result = soapResponse[start..end].Trim();
                 return Ok(new NumberToWordsResponse { NumberToWordsResult = result });
             }
             else
@@ -48,15 +52,90 @@ $@"<?xml version=""1.0"" encoding=""utf-8""?>
                 return BadRequest("Invalid SOAP Response");
             }
         }
+
+        [HttpPost]
+        [Route("ListOfLanguagesByName")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ListOfLanguagesByNameResponse))]
+        public async Task<IActionResult> ListOfLanguagesByName([FromBody] ListOfLanguagesByNameRequest request)
+        {
+            string soapRequestBody =
+        $@"<?xml version=""1.0"" encoding=""utf-8""?>
+<soap:Envelope xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"">
+    <soap:Body>
+        <ListOfLanguagesByName xmlns=""http://www.oorsprong.org/websamples.countryinfo"">
+        </ListOfLanguagesByName>
+    </soap:Body>
+</soap:Envelope>";
+
+            using HttpClient httpClient = new HttpClient();
+            using StringContent content = new StringContent(soapRequestBody, Encoding.UTF8, "text/xml");
+            using HttpResponseMessage response = await httpClient.PostAsync("http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso", content);
+            string soapResponse = await response.Content.ReadAsStringAsync();
+
+            var doc = new XmlDocument();
+            doc.LoadXml(soapResponse);
+
+            var namespaces = new XmlNamespaceManager(doc.NameTable);
+            namespaces.AddNamespace("m", "http://www.oorsprong.org/websamples.countryinfo");
+
+            var languageNodes = doc.SelectNodes("//m:tLanguage", namespaces);
+
+            var result = new ListOfLanguagesByNameResponse
+            {
+                Languages = new List<ListOfLanguagesByNameResponse.Language>()
+            };
+
+            foreach (XmlNode node in languageNodes)
+            {
+                var sISOCode = node.SelectSingleNode("m:sISOCode", namespaces)?.InnerText;
+                var sName = node.SelectSingleNode("m:sName", namespaces)?.InnerText;
+
+                result.Languages.Add(new ListOfLanguagesByNameResponse.Language
+                {
+                    sISOCode = sISOCode,
+                    sName = sName
+                });
+            }
+
+            return Ok(result);
+        }
+
+
     }
+
+
+
+    //STEP 1 -> TAKE XML / XSD AND GENERATE BELOW CLASSES. -> STEP 2 IMPLEMENT API ENDPOINT(LANGUAGES BY NAME). (ALSO GENERATED) --> NEXT BUILD AND RUN THE GENERATED PROGRAM USING SCRIPT OR SOMETHING?
+
+    public class ListOfLanguagesByNameRequest
+    {
+        // This class can be empty since no input parameters are needed for the SOAP request
+    }
+
+    public class ListOfLanguagesByNameResponse
+    {
+        public List<Language> Languages { get; set; }
+
+        public class Language
+        {
+            [Required]
+            [MaxLength(5)]
+            public string sISOCode { get; set; }
+            public string sName { get; set; }
+        }
+    }
+
 
     public class NumberToWordsRequest
     {
+        [Required]
+        [MaxLength(Int32.MaxValue)]
+        [MinLength(Int32.MinValue)]
         public int ubiNum { get; set; }
     }
 
     public class NumberToWordsResponse
     {
-        public string NumberToWordsResult { get; set; }
+        public string? NumberToWordsResult { get; set; }
     }
 }
